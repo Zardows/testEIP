632e5b1c312f30c1282c0faabf2439f3a7706204
Zardows <47741919+Zardows@users.noreply.github.com>
2023-05-04 11:53:48 +0400

    Create test_my_module.py

---
+++ test_my_module.py
@@ -0,0 +1,7 @@
+import my_module
+
+def test_multiply():
+    assert my_module.multiply(2, 3) == 6
+    assert my_module.multiply(5, 0) == 0
+    assert my_module.multiply(-1, 1) == -1
+    assert my_module.multiply(-1, -1) == 1
